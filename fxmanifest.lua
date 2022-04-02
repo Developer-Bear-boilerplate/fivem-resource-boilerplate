115ddac1d1f184bb52ff5b5bbed67f4923e09e14
Bear <44989435+Developer-Bear@users.noreply.github.com>
2022-04-01 21:14:32 -0500

    Initial commit

---
+++ fxmanifest.lua
@@ -0,0 +1,21 @@
+fx_version 'cerulean'
+
+game 'gta5'
+
+author ''
+
+description ''
+
+version '1.0.0'
+
+shared_scripts {
+    'script-config.lua'
+}
+
+client_scripts {
+    'script-client.lua'
+}
+
+server_scripts {
+    'script-server.lua'
+}
